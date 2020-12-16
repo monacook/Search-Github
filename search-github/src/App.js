da6b29bad9c4c85c69a8d15ec7e654738ee3f5cf
monacook <cook.mona@gmail.com>
2020-12-16 16:52:47 -0700

    Removed materialcss and set up form

--- search-github/src/App.js
+++ search-github/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
 // import { Octokit } from "@octokit/core";
 
 class App extends React.Component {
-
   render(){
     return (
       <div className="ui segment">
                 <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <h1>Github Search</h1>
-                        <Input type="text" value="" placeholder="Search Github"/>
-                        <Button variant="outlined" color="primary" size="small">Search</Button>
+                        <input type="text" value="" placeholder="Search Github" size="50"/>
+                        <button variant="outlined" color="primary" size="small">Search</button>
                     </div>
                 </form>
             </div>
