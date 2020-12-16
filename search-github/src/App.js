d48214039dfb4dd01931ac0b78d8fd20968b8df9
monacook <cook.mona@gmail.com>
2020-12-16 14:13:08 -0700

    set up appjs and downloaded octokit/core

--- search-github/src/App.js
+++ search-github/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-  //state here
+  constructor() {
+    super();
+    this.state={
+      error: null,
+      repos: []
+    }
+  }
 
   // function that gets the api request from Github search api
   // store that into state by using setState
   render(){
     
     return (
-      
+      <p> hello</p>
     )
   }
 }
