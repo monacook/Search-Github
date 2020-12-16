4347a58c9aa4be0239dd249bb8182bd62ffb2b90
monacook <cook.mona@gmail.com>
2020-12-16 13:38:47 -0700

    added stuff

--- search-github/src/App.js
+++ search-github/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class App extends React.Component {
+  //state here
+
+  // function that gets the api request from Github search api
+  // store that into state by using setState
   render(){
+    
     return (
-      <p>hello!</p>
+      
     )
   }
 }
