516d382cfab667f85bc9314c5f51d81c5cd1ca18
monacook <cook.mona@gmail.com>
2020-12-16 18:36:28 -0700

    set up on change function and on submit

--- search-github/src/App.js
+++ search-github/src/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
-// import { Octokit } from "@octokit/core";
 
 class App extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    value:''
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({value: e.target.value})
+    // console.log(this.state.value)
+  }
+
+  onSubmit = (e) => {
+   e.preventDefault()
+   console.log("hello")
+   this.props.onSubmit(this.state.value)
+  }
+  
+
   render(){
+    console.log(this.state)
     return (
       <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <h1>Github Search</h1>
-                        <input type="text" value="" placeholder="Search Github" size="50"/>
-                        <button variant="outlined" color="primary" size="small">Search</button>
-                    </div>
-                </form>
-            </div>
-      
+        <form className="ui form">
+          <div className="field">
+            <h1>Github Search</h1>
+            <input type="text" value={this.state.value} placeholder="Search Github" size="50" onChange={this.onChange}/>
+            <button onSubmit={this.onSubmit} variant="outlined" color="primary" size="small">Search</button>
+          </div>
+        </form>
+      </div> 
     )
   }
 }
