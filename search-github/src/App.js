a8311063923497e111dc4a2407f18c4e18d23648
monacook <cook.mona@gmail.com>
2020-12-17 21:09:25 -0700

    added a header

--- search-github/src/App.js
+++ search-github/src/App.js
@@ -1,39 +1,44 @@
-import React from 'react';
+// Props go down to child
+import React, { Component } from 'react';
+import Header from './components/Header';
 
-class App extends React.Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-    value:''
-    };
-  }
-
-  onChange = (e) => {
-    this.setState({value: e.target.value})
-    // console.log(this.state.value)
-  }
-
-  onSubmit = (e) => {
-   e.preventDefault()
-   console.log("hello")
-   this.props.onSubmit(this.state.value)
-  }
-  
-
-  render(){
-    console.log(this.state)
-    return (
-      <div className="ui segment">
-        <form className="ui form">
-          <div className="field">
-            <h1>Github Search</h1>
-            <input type="text" value={this.state.value} placeholder="Search Github" size="50" onChange={this.onChange}/>
-            <button onSubmit={this.onSubmit} variant="outlined" color="primary" size="small">Search</button>
-          </div>
-        </form>
-      </div> 
+class App extends Component {
+render() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
     )
   }
 }
 
 export default App;
+
+  // <div className="ui segment">
+  //   <form onSubmit={this.formSubmit} className="ui form">
+  //       <h1>Github Search</h1>
+  //       <input type="text" value={this.state.value} placeholder="Search Github" size="50" onChange={this.onChange}/>
+  //       <button onSubmit={this.onSubmit} variant="outlined" color="primary" size="small">Search</button>
+  //     </div>
+  //   </form>
+  // </div> 
+
+// class App extends React.Component {
+//   constructor() {
+//   super();
+//   this.state = {
+//     value:''
+//     };
+//   }
+
+//   onChange = (e) => {
+//     this.setState({value: e.target.value})
+//   }
+
+//   formSubmit = (e) => {
+//    e.preventDefault()
+//    console.log(this.state.value)
+//   }
+  
+
+//   render(){
---
+++ search-github/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header class="page-header">
+      <h1>Github Search</h1>
+    </header>
+  )
+}
+
+export default Header;
