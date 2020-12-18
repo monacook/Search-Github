9831bb8794ef4d4086091f069c91ce1001cfa7aa
monacook <cook.mona@gmail.com>
2020-12-18 15:51:46 -0700

    header component and api setup ish...

--- search-github/src/App.js
+++ search-github/src/App.js
@@ -1,12 +1,48 @@
 // Props go down to child
 import React, { Component } from 'react';
 import Header from './components/Header';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+  super();
+  this.state = {
+    value:''
+    };
+  }
+ 
+// Import the api
+ async getUsers = (term) => {
+   const response = await axios.get('https://api.github.com/search/users?q=repository', {
+     q: 'q'
+   }
+  )
+}
+
+// Search Submit 
+
+
+// change that is happening right here
+   onChange = (e) => {
+    this.setState({value: e.target.value})
+  }
+
+  // form to submit right here
+
+  formSubmit = (e) => {
+   e.preventDefault()
+   console.log(this.state.value)
+  }
+
+  // all for it to render right here including forn
 render() {
   return (
     <div className="App">
       <Header />
+        <form onSubmit={this.formSubmit} className="ui form">
+          <input type="text" value={this.state.value} placeholder="Search Github" size="50" onChange={this.onChange}/>
+          <button onSubmit={this.onSubmit} variant="outlined" color="primary" size="small">Search</button>
+        </form>
     </div>
     )
   }
@@ -14,14 +50,26 @@ render() {
 
 export default App;
 
-  // <div className="ui segment">
-  //   <form onSubmit={this.formSubmit} className="ui form">
-  //       <h1>Github Search</h1>
-  //       <input type="text" value={this.state.value} placeholder="Search Github" size="50" onChange={this.onChange}/>
-  //       <button onSubmit={this.onSubmit} variant="outlined" color="primary" size="small">Search</button>
-  //     </div>
-  //   </form>
-  // </div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const onChange = (e) => {
+//        this.setState({value: e.target.value})
+//   }
+// }
 
 // class App extends React.Component {
 //   constructor() {
--- search-github/src/components/Header.js
+++ search-github/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header class="page-header">
+    <header className="page-header">
       <h1>Github Search</h1>
     </header>
   )
