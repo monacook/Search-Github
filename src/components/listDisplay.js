8ba58d9b50e9128f5f521a8a614b951fb231989a
monacook <cook.mona@gmail.com>
2020-12-29 13:25:16 -0700

    styling and updated yarn

--- src/components/listDisplay.js
+++ src/components/listDisplay.js
@@ -3,15 +3,14 @@ import React from 'react';
 const ListDisplay = (props) => {
     const listDisplay = props.value.map(user => {
             return <div key={user.login}>
-                <p>{user.login}</p>
                 <a href={user.html_url}>
                 <img src={user.avatar_url} alt=""></img>
+                <p>{user.login}</p>
                 </a>
             </div>
     })
     
     return <div>{listDisplay}</div>
 }
-
 
 export default ListDisplay;
